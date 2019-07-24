4b374e5e7d3a1addbf2fe80f40683373e06eea0b
Oskar Damkjaer <oskar.damkjaer@zayarecruiting.de>
2019-07-24 15:38:14 +0200

    show video

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,11 @@
-// We need to import the CSS so that webpack will load it.
-// The MiniCssExtractPlugin is used to separate it out into
-// its own CSS file.
 import css from "../css/app.css"
-
-// webpack automatically bundles all modules in your
-// entry points. Those entry points can be configured
-// in "webpack.config.js".
-//
-// Import dependencies
-//
 import "phoenix_html"
+import Player from "./player"
 
-// Import local files
-//
-// Local files can be imported directly using relative paths, for example:
-// import socket from "./socket"
+console.log("one")
+
+const video = document.getElementById("video")
+if(video) {
+    console.log("two")
+    Player.init(video.id, video.getAttribute("data-player-id"), () => { console.log("player ready!") })
+}
